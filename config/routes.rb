e040646821ae6466006d4141b48af77f4713074c
Luiz Felipe <fffelippe51413121lippe@gmail.com>
2025-03-27 00:52:07 -0300

    Route and controller create

--- config/routes.rb
+++ config/routes.rb
@@ -7,17 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get "/products", to: "products#index"
+  resources :products
 
-  get "/products/new", to: "products#new"
-  post "/products", to: "products#create"
-  
-  get "/products/:id", to: "products#show"
-  
-  get "/products/:id/edit", to: "products#edit"
-  patch "/products/:id", to: "products#update"
-  put "/products/:id", to: "products#update"
-  
-  delete "/products/:id", to: "products#destroy"
   
 end
